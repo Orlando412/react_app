c408c8785c8caf4784edf32b981d1566f1a39391
Orlando Adams <ojadams@student.fullsail.edu>
2024-06-07 18:26:17 -0400

    start of react app

---
+++ src/components/navBar.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+function navBar() {
+  return (
+    <>
+      <navBar className="navbar">
+        <div className= "navbar-container">
+          
+        </div>
+      </navBar>
+    </>
+  )
+}
+
+export default navBar
